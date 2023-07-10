cd69c7a459813496f8e0c0eecc0ee7e3869adf5b
MykolaMatsko <mykola.matsko@outlook.com>
2023-07-11 00:01:28 +0300

    next

---
+++ Circle/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CircleP58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double area;
+
+            radius = 10.0;
+            area = radius * radius * 3.1416;
+
+            Console.WriteLine("Area equals " + area);
+        }
+    }
+}
---
+++ Example3p57/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Example3p57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ivar;
+            double dvar;
+
+            ivar = 100;
+
+            dvar = 100.0;
+
+            Console.WriteLine("ivar before: " + ivar);
+            Console.WriteLine("dvar before: " + dvar);
+
+            Console.WriteLine();
+
+            ivar = ivar / 3;
+            dvar = dvar / 3;
+
+            Console.WriteLine("ivar after: " + ivar);
+            Console.WriteLine("dvar after: " + dvar);
+
+        }
+    }
+}
