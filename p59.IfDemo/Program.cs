908ce986160c489751e653d0ccf8b31dd905db0a
MykolaMatsko <mykola.matsko@outlook.com>
2023-07-11 00:03:52 +0300

    next

---
+++ p59.IfDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace p59.IfDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
