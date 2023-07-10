5c3605b330d59451129566198380fa6208b6ce72
MykolaMatsko <mykola.matsko@outlook.com>
2023-07-10 23:43:37 +0300

    Add project files.

---
+++ Example2p55/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Example2p55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 100;
+            Console.WriteLine("x contains " + x);
+
+            y = x / 2;
+            Console.WriteLine("y contains x / 2: " + x / 2);
+            Console.WriteLine(y);
+        }
+    }
+}
